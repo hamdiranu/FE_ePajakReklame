21b240fb9e82f0765792044ecc55d26605905fde
Hamdi Ranuharja <hamdi@alterra.id>
2020-02-05 18:50:02 +0700

    add payer marker

--- src/pages/petaPayer.jsx
+++ src/pages/petaPayer.jsx
@@ -4,19 +4,52 @@ import "../styles/stylePetaPayer.css";
 import { withRouter } from "react-router-dom";
 import { connect } from "unistore/react";
 import { actions } from "../store";
-// // import NavigasiSurveyor from "../components/navigasiSurveyor";
-// import "mapbox-gl/dist/mapbox-gl.css";
-// import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import mapboxgl from "mapbox-gl";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
-// const token =
-//   "pk.eyJ1IjoiaGFtZGlyYW51IiwiYSI6ImNrNjkxdjF4aTBiOGczbGxqOWdocnhrN3kifQ.4x6Q9f7hcT-xSqZv4plNxA";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaGFtZGlyYW51IiwiYSI6ImNrNjkxdjF4aTBiOGczbGxqOWdocnhrN3kifQ.4x6Q9f7hcT-xSqZv4plNxA";
 
 class PetaPayer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 112.62163667136366,
+      lat: -7.960512177966962,
+      zoom: 18
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+
   render() {
     return (
-      <div>
-        <div id="map"></div>
-        <pre id="coordinates" class="coordinates"></pre>
+      <div className="kotakInputLokasi">
+        <div className="markerLokasi">
+          <FaMapMarkerAlt />
+        </div>
+        <div className="sidebarStyle">
+          <div>
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
+            {this.state.zoom}
+          </div>
+        </div>
+        <div ref={el => (this.mapContainer = el)} className="mapContainer" />
       </div>
     );
   }
